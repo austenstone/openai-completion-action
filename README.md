0e0c4fd6e3fc1f9bceb1be88c9f830db59abd297
Austen Stone <austenstone@github.com>
2022-01-27 10:54:00 -0500

    🚀

---
+++ README.md
@@ -0,0 +1,44 @@
+# OpenAI Completion Action 🤖
+You input some text as a prompt, and the model will generate a text completion that attempts to match whatever context or pattern you gave it.
+
+For example, if you input
+`Write a tagline for an ice cream shop.`, the model will generate a response like `Sweet treats for everyone!`.
+
+## Usage
+The action requires: 
+- Input called `prompt` which is a string that the model will use to generate a completion.
+- [OpenAI API Key](https://beta.openai.com/account/api-keys) as an enviroment variable called `OPENAI_API_KEY`
+
+To see all other inputs visit [OpenAI Api Reference - Completions](https://beta.openai.com/docs/api-reference/completions/create)
+
+The output of the action is a JSON string that can be parsed. The resulting JSON object will contain the response. Use `.choices[0].text` to get the first choice's text.
+
+This example is a manual dispatch action that takes the prompt as input and prints out the response:
+```yml
+name: OpenAI Completion
+
+on:
+  workflow_dispatch:
+    inputs:
+      prompt:
+        description: The prompt to generate completions for.
+        required: true
+
+jobs:
+  openai_completion:
+    runs-on: ubuntu-latest
+    name: OpenAI
+    steps:
+      - uses: austenstone/openai-completion-action@master
+        name: OpenAI Completion
+        id: openai_completion
+        with:
+          prompt: ${{ github.event.inputs.prompt }}
+        env:
+          OPENAI_API_KEY: ${{ secrets.OPENAI_API_KEY }}
+
+      - uses: actions/github-script@v5
+        name: Print the response text
+        with:
+          script: core.info(JSON.parse('${{ steps.openai_completion.outputs.response }}').choices[0].text)
+```
---
+++ index.js
@@ -0,0 +1,73 @@
+
+const core = require('@actions/core');
+const { Configuration, OpenAIApi } = require("openai");
+
+const getCoreInput = (name) => {
+    const input = core.getInput(name);
+    return input.length === 0 ? undefined : input;
+}
+
+const engineId = getCoreInput('engineId') || 'text-davinci-001';
+const prompt = getCoreInput('prompt');
+const max_tokens = parseInt(getCoreInput('max_tokens'));
+const temperature = getCoreInput('temperature');
+const top_p = getCoreInput('top_p');
+const n = getCoreInput('n');
+const stream = getCoreInput('stream');
+const logprobs = getCoreInput('logprobs');
+const echo = getCoreInput('echo');
+const stop = getCoreInput('stop');
+const presence_penalty = getCoreInput('presence_penalty');
+const frequency_penalty = getCoreInput('frequency_penalty');
+const best_of = getCoreInput('best_of');
+const logit_bias = getCoreInput('logit_bias');
+
+if (!prompt) {
+    core.setFailed('No prompt provided');
+    return;
+}
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+    // organization: 'string',
+    // username: 'string',
+    // password: 'string',
+    // accessToken: 'string',
+    // basePath: 'string',
+    // baseOptions: {},
+    // formDataCtor: {}
+});
+const openai = new OpenAIApi(configuration);
+
+const payload = {
+    prompt,
+    max_tokens,
+    temperature,
+    top_p,
+    n,
+    stream,
+    logprobs,
+    echo,
+    stop,
+    presence_penalty,
+    frequency_penalty,
+    best_of,
+    logit_bias
+};
+
+core.info(`Request using engineId: ${engineId}
+${JSON.stringify(payload, null, 2)}`);
+
+openai.createCompletion(engineId, payload).then((response) => {
+    const data = response.data;
+    data.choices = data.choices.map((choice) => {
+        choice.text = choice.text.replace(/(?:\r\n|\r|\n)/g, '')
+        return choice;
+    });
+    core.info(`Response:
+${JSON.stringify(data, null, 2)}`);
+    core.setOutput('response', JSON.stringify(data));
+}).catch((err) => {
+    core.setFailed(err);
+    return;
+});
