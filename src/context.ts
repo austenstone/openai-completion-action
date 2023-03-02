c773f6999749cb4bae96c0b38bb5ad7402bf4604
Austen Stone <austenstone@github.com>
2023-03-02 12:04:24 -0500

    cleanup response

--- src/context.ts
+++ src/context.ts
@@ -72,6 +72,12 @@ const run = async (): Promise<void> => {
   core.info(`Request using model: ${payload.model}\n${JSON.stringify(payload, null, 2)}`);
   const response = await openai.createChatCompletion(payload);
   const data = response.data;
+  data.choices = data.choices?.map((choice) => {
+    if (choice.message?.content) {
+      choice.message.content = choice.message?.content?.replace(/(?:\r\n|\r|\n)/g, '');
+    }
+    return choice;
+  });
   core.setOutput('response', JSON.stringify(data));
 };
 
